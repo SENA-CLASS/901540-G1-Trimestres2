c4a02f2ab7a4011f995c1114ba76086ff3a412a5
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2015-08-06 14:34:17 -0500

    Clases
    se explico la estructura de maven y ant para proyectos java

---
+++ java/POO/Ejemplo5Maven/src/main/java/edu/co/sena/ejemplo/Hola.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.ejemplo;
+
+/**
+ *
+ * @author hernando
+ */
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+    
+}
